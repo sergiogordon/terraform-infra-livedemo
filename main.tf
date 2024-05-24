9fa6f553c1aef3c2b43c65fb5aea7e16ddf47362
sergiogordon <gordon.sergio9@gmail.com>
2024-05-23 21:43:29 -0500

    Fix security group configuration for EC2 instance

--- main.tf
+++ main.tf
@@ -87,10 +87,10 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "web" {
-  ami             = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI for us-east-2
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public.id
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami                  = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI for us-east-2
+  instance_type        = "t2.micro"
+  subnet_id            = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   user_data = <<-EOF
               #!/bin/bash
