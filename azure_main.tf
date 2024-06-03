28971327654b3d38bac56b7a4de7e72f3949f4aa
sergiogordon <13407640+sergiogordon@users.noreply.github.com>
2024-06-03 17:51:51 -0500

    changed terraform to tf

--- azure_main.tf
+++ azure_main.tf
@@ -5,22 +5,24 @@ provider "azurerm" {
 
 # Generate a random string to use as a suffix
 resource "random_string" "random_suffix" {
-  length  = 4
+  length  = 6
   special = false
   upper   = false
+  lower   = true
+  numeric = true
 }
 
 # Create a resource group to organize resources
-resource "azurerm_resource_group" "terraform_rg" {
-  name     = "terraform-resources-env-${random_string.random_suffix.result}"
+resource "azurerm_resource_group" "tf_rg" {
+  name     = "tf-resources-env-${random_string.random_suffix.result}"
   location = "East US"
 }
 
 # Create a storage account within the resource group
-resource "azurerm_storage_account" "terraform_storage_account" {
-  name                     = "terraformresourcebucket${random_string.random_suffix.result}"
-  resource_group_name      = azurerm_resource_group.terraform_rg.name
-  location                 = azurerm_resource_group.terraform_rg.location
+resource "azurerm_storage_account" "tf_storage_account" {
+  name                     = "tfresrc${random_string.random_suffix.result}"
+  resource_group_name      = azurerm_resource_group.tf_rg.name
+  location                 = azurerm_resource_group.tf_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -32,23 +34,23 @@ resource "azurerm_storage_account" "terraform_storage_account" {
 }
 
 # Create a container within the storage account
-resource "azurerm_storage_container" "terraform_container" {
+resource "azurerm_storage_container" "tf_container" {
   name                  = "content"
-  storage_account_name  = azurerm_storage_account.terraform_storage_account.name
+  storage_account_name  = azurerm_storage_account.tf_storage_account.name
   container_access_type = "private"
 }
 
 # Output the name of the storage account
 output "storage_account_name" {
-  value = azurerm_storage_account.terraform_storage_account.name
+  value = azurerm_storage_account.tf_storage_account.name
 }
 
 # Output the name of the storage container
 output "container_name" {
-  value = azurerm_storage_container.terraform_container.name
+  value = azurerm_storage_container.tf_container.name
 }
 
 # Output the name of the resource group
 output "resource_group_name" {
-  value = azurerm_resource_group.terraform_rg.name
+  value = azurerm_resource_group.tf_rg.name
 }
